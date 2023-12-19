17ab80382d2f3109279e1a8508e891009fdff147
claryssayuanwie <claryuanwie@gmail.com>
2023-12-19 11:06:45 -0800

    Updated logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+    import {onMount} from 'svelte';
+    
+</script>
+
 <h1>Hello!</h1>
-<p>Welcome to studyclock! Start a study session!</p>
+<p>Welcome to studyclock! Start a study session!
+    You can check your stats here.
+</p>
